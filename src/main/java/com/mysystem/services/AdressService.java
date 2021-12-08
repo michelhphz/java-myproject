2d4e4860ae138e721e59559d6b7fe24ffed1708f
Michel Pech <michelhphz@hotmail.com>
2021-12-08 20:47:10 -0300

    First configure AdressService

--- src/main/java/com/mysystem/services/AdressService.java
+++ src/main/java/com/mysystem/services/AdressService.java
@@ -1,5 +1,43 @@
 package com.mysystem.services;
 
-public class AdressService {
+import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mysystem.entities.Adress;
+import com.mysystem.repositories.AdressRepository;
+
+@Service
+public class AdressService {
+	
+	@Autowired
+	private AdressRepository adressRepository;
+		
+	public List<Adress> findAll(){
+		List<Adress> list = adressRepository.findAll();
+		return list;
+	}
+	
+	public Adress findById(Adress adress) {
+		Adress adressFind = adressRepository.getById(adress.getId());
+		return adressFind;
+	}
+	
+	@Transactional
+	public void insert(Adress adress) {
+		adressRepository.save(adress);
+	}
+	
+	@Transactional
+	public void delete(Adress adress) {
+		adressRepository.delete(adress);
+	}
+	
+	@Transactional
+	public void update(Adress adress) {
+		adressRepository.save(adress);
+	}
 }
