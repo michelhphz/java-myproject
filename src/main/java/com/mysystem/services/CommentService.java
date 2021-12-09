cbc9f3df7693a1361f63c09a56e7262aaca1c75c
Michel Pech <michelhphz@hotmail.com>
2021-12-08 21:12:05 -0300

    First configure CommentService and ItemService

--- src/main/java/com/mysystem/services/CommentService.java
+++ src/main/java/com/mysystem/services/CommentService.java
@@ -1,5 +1,44 @@
 package com.mysystem.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mysystem.entities.Comment;
+import com.mysystem.repositories.CommentRepository;
+
+@Service
 public class CommentService {
 
+	@Autowired
+	private CommentRepository commentRepository;
+	
+	public List<Comment> findAll(){
+		List<Comment> list = commentRepository.findAll();
+		return list;
+	}
+	
+	public Optional<Comment> findById(Long id){
+		Optional<Comment> commentFind = commentRepository.findById(id);
+		return commentFind;
+	}
+	
+	@Transactional
+	public void insert(Comment Comment) {
+		commentRepository.save(Comment);
+	}
+	
+	@Transactional
+	public void delete(Comment Comment) {
+		commentRepository.delete(Comment);
+	}
+	
+	@Transactional
+	public void update(Comment Comment) {
+		commentRepository.save(Comment);
+	}	
 }
--- src/main/java/com/mysystem/services/ItemService.java
+++ src/main/java/com/mysystem/services/ItemService.java
@@ -1,5 +1,44 @@
 package com.mysystem.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mysystem.entities.Item;
+import com.mysystem.repositories.ItemRepository;
+
+@Service
 public class ItemService {
 
+	@Autowired
+	private ItemRepository itemRepository;
+	
+	public List<Item> findAll(){
+		List<Item> list = itemRepository.findAll();
+		return list;
+	}
+	
+	public Optional<Item> findById(Long id){
+		Optional<Item> ItemFind = itemRepository.findById(id);
+		return ItemFind;
+	}
+	
+	@Transactional
+	public void insert(Item Item) {
+		itemRepository.save(Item);
+	}
+	
+	@Transactional
+	public void delete(Item Item) {
+		itemRepository.delete(Item);
+	}
+	
+	@Transactional
+	public void update(Item Item) {
+		itemRepository.save(Item);
+	}	
 }
